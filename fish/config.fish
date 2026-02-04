e0db6efc710fc984e80e2a8c0913d8e70ca6391e
mofumail <189025105+mofumail@users.noreply.github.com>
2026-02-04 06:02:40 +0100

    Add fish, anyrun, kitty configs and symlink all dotfiles

---
+++ fish/config.fish
@@ -0,0 +1,20 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+starship init fish | source
+
+# uv
+source "$HOME/.local/share/../bin/env.fish"
+
+# pyenv
+set -gx PYENV_ROOT "$HOME/.pyenv"
+fish_add_path "$PYENV_ROOT/bin"
+pyenv init - fish | source
+
+# rust
+source "$HOME/.cargo/env.fish"
